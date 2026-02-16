932fa08854ff20e38dab443071f0d6d45a0629e6
DeivisStasuls <deivis.stasuls@gmail.com>
2026-02-16 13:40:45 +0200

    new files

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,8 +8,15 @@
 <body>
     <h1>About page </h1>
     <a href="/"> welcome</a>
-    <br>
-    {{$name}}
-    <?php echo $name ?>
+    <h1>Name</h1>
+    {{ $person->name }}
+    <h2>Email</h2>
+    {{ $person->email }}
+    <h3>Languages</h3>
+    <ul>
+    @foreach ($person->languages as $language)
+    <li>{{ $language }}</li>
+@endforeach
+</ul>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,28 @@ Route::get('/about', function () {
     //     'email'=>'antra@ventspilstehnikums.lv',
     //     'languages'=>['PHP','Python','Javascript']
     // ];
+    class Person {
+        public $name;
+        public $email;
+        public $languages;
+        
+//   function __construct($name, $email, $languages) {
+//     $this->name = $name;
+//     $this->email = $email;
+//     $this->languages = $languages;
+//     }
+//     function get_details() {
+//   echo "Name: " . $this->name . " email: " . $this->email ."languages: " . $this->languages;
+// }
+    };
+$person1 = new Person;
+$person1->name ='Antra';
+$person1->email='antra@ventspilstehnikums.lv';
+$person1->languages=['PHP','Python','Javascript'];
 
     $testName = "Jānis";
 
-    return view('about', ['name' => $testName]);
+    return view('about', ['person' => $person1]);
 });
 
 
